e1f39584e078af33fe2f6b753bae95bc617bb821
Bianca Santana Souza <bsantana@c1r6p1.42sp.org.br>
2024-09-04 10:53:56 -0300

    modified: megaphone fuction

--- LISTA 0/ex00/megaphone.cpp
+++ LISTA 0/ex00/megaphone.cpp
@@ -6,25 +6,31 @@
 /*   By: bsantana <bsantana@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 14:47:49 by bsantana          #+#    #+#             */
-/*   Updated: 2024/09/03 18:42:23 by bsantana         ###   ########.fr       */
+/*   Updated: 2024/09/04 10:53:13 by bsantana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 
-void megaphone(const std::string& str)
+void megaphone(std::string& str)
 {
-    std::cout << "Argumento: " << str << std::endl;
+    for (size_t i = 0; i < str.length(); i++)
+    {
+        str[i] = toupper(static_cast<unsigned char>(str[i]));
+    }
+    std::cout << str << std::endl;
 }
 
 int main(int argc, char **argv)
 {
+    std::string argument;
+
     if (argc == 1)
         std::cout << "*LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
     for (int i = 1; i < argc; i++)
     {
-        std::string argument = argv[i];
+        argument = argv[i];
         megaphone(argument);
     }
     return 0;
