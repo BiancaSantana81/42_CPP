70d39596ad1fe8d440ab4a79dcd6325543caed1d
Bianca Santana Souza <bsantana@c1r6p1.42sp.org.br>
2024-09-04 13:51:50 -0300

    new megaphone with class

--- LISTA 0/ex00/megaphone.cpp
+++ LISTA 0/ex00/megaphone.cpp
@@ -6,30 +6,39 @@
 /*   By: bsantana <bsantana@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 14:47:49 by bsantana          #+#    #+#             */
-/*   Updated: 2024/09/04 10:56:41 by bsantana         ###   ########.fr       */
+/*   Updated: 2024/09/04 13:40:02 by bsantana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 
-void megaphone(std::string& str)
+class Megaphone
 {
-    for (size_t i = 0; i < str.length(); i++)
-        str[i] = toupper(str[i]);
-    std::cout << str << std::endl;
-}
+    public:
+
+    void TransformString(std::string str)
+    {
+        for (size_t i = 0; i < str.length(); i++)
+            str[i] = toupper(str[i]);
+        std::cout << str;
+    }
+};
 
 int main(int argc, char **argv)
 {
-    std::string argument;
-
     if (argc == 1)
         std::cout << "*LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-    for (int i = 1; i < argc; i++)
+    else
     {
-        argument = argv[i];
-        megaphone(argument);
+        for (int i = 1; i < argc; i++)
+        {
+            std::string argument;
+            argument = argv[i];
+            Megaphone megaphone;
+            megaphone.TransformString(argument);
+        }
+        std::cout << std::endl;
     }
-    return 0;
+    return (0);
 }
