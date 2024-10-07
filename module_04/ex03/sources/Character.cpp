f13239806a69ec9a6504785e8edca8ff100199b0
bsantana <biancasantana81@hotmail.com>
2024-10-07 16:41:47 -0300

    without leaks and seg fault

--- module_04/ex03/sources/Character.cpp
+++ module_04/ex03/sources/Character.cpp
@@ -6,7 +6,7 @@
 /*   By: bsantana <bsantana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:22:58 by bsantana          #+#    #+#             */
-/*   Updated: 2024/10/07 15:35:51 by bsantana         ###   ########.fr       */
+/*   Updated: 2024/10/07 16:29:10 by bsantana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,25 +65,27 @@ void Character::setName(std::string newName)
     _name = newName;
 }
 
-void Character::equip(AMateria *m)
+void Character::equip(AMateria* m)
 {
-    if (!m)
+    if (m == NULL)
+    {
+        std::cout << "Cannot equip a null materia!" << std::endl;
         return ;
-    for (int i = 0; i < MAX_STOCK; i++)
+    }
+    for (int i = 0; i < MAX_STOCK; ++i)
     {
         if (_stock[i] == NULL)
         {
             _stock[i] = m;
-            std::cout << _name << " equipped " << m->getType() << "." << std::endl;
-            break ;
+            return ;
         }
     }
-    std::cout << _name << "'s stock is full! Cannot equip more materias." << std::endl;
+    std::cout << "Stock is full! Cannot equip more materia." << std::endl;
 }
 
 void Character::unequip(int idx)
 {
-    if (idx < 0 || idx >= MAX_STOCK || _stock[idx] == NULL)
+    if (idx < 0 || idx >= MAX_STOCK)
         return ;
     std::cout << _name << " unequipped " << _stock[idx]->getType() << "." << std::endl;
     _stock[idx] = NULL;
@@ -110,23 +112,3 @@ Character::~Character(void)
         }
     }
 }
-
-
-// Character::~Character(void)
-// {
-//     std::cout << "Character: Destructor called." << std::endl;
-//     for (int i = 0; i < MAX_STOCK; ++i)
-//     {
-//         std::cout << "Deleting _stock[" << i << "]" << std::endl;
-//         if (_stock[i] != NULL)
-//         {
-//             std::cout << "_stock[" << i << "] is not NULL, deleting..." << std::endl;
-//             delete _stock[i];
-//             _stock[i] = NULL;
-//         }
-//         else
-//         {
-//             std::cout << "_stock[" << i << "] is NULL, skipping..." << std::endl;
-//         }
-//     }
-// }
--- module_04/ex03/sources/main.cpp
+++ module_04/ex03/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bsantana <bsantana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 16:27:50 by bsantana          #+#    #+#             */
-/*   Updated: 2024/10/07 15:37:44 by bsantana         ###   ########.fr       */
+/*   Updated: 2024/10/07 16:39:57 by bsantana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,33 @@
 #include "../includes/IMateriaSource.hpp"
 #include "../includes/MateriaSource.hpp"
 
+// int main(void)
+// {
+//     IMateriaSource* src = new MateriaSource();
+//     src->learnMateria(new Ice());
+//     src->learnMateria(new Cure());
+//     ICharacter* me = new Character("me");
+//     AMateria* tmp;
+//     tmp = src->createMateria("ice");
+//     me->equip(tmp);
+//     tmp = src->createMateria("cure");
+//     me->equip(tmp);
+//     ICharacter* bob = new Character("bob");
+//     me->use(0, *bob);
+//     me->use(1, *bob);
+//     delete bob;
+//     delete me;
+//     delete src;
+//     return 0;
+// }
+
+
 int main(void)
 {
-    IMateriaSource* src = new MateriaSource();
-    src->learnMateria(new Ice());
-    src->learnMateria(new Cure());
-    ICharacter* me = new Character("me");
-    AMateria* tmp;
-    tmp = src->createMateria("ice");
-    me->equip(tmp);
-    tmp = src->createMateria("cure");
-    me->equip(tmp);
-    ICharacter* bob = new Character("bob");
-    me->use(0, *bob);
-    me->use(1, *bob);
-    delete bob;
-    delete me;
-    delete src;
+    ICharacter *guy = new Character("Bianca"); // Cria um novo personagem
+    AMateria *m1 = new Ice(); // Cria uma nova matéria do tipo Ice
+    
+    guy->equip(m1); // Tenta equipar a matéria
+    delete guy; // Libera a memória do personagem
     return 0;
 }
