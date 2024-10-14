4be34f41dc68768de607cddbd52ad197a8e22dee
Bianca Santana Souza <bsantana@c1r4p2.42sp.org.br>
2024-10-14 10:24:21 -0300

    init ex02

---
+++ module_05/ex02/includes/Bureaucrat.hpp
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsantana <bsantana@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/10 13:22:21 by bsantana          #+#    #+#             */
+/*   Updated: 2024/10/11 15:34:14 by bsantana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#define BRIGHT_YELLOW "\033[93m"
+#define BRIGHT_CYAN   "\033[96m"
+#define BRIGHT_RED    "\033[91m"
+#define RESET   "\033[0m"
+
+#include <iostream>
+#include <ctime>
+#include <string>
+
+class Form;
+
+class Bureaucrat
+{
+    private:
+
+    const std::string _name;
+    int         _grade;
+
+    public:
+
+    // constructors 
+
+    Bureaucrat(void); // construtor padrão 
+    Bureaucrat(std::string name, int grade); // construtor padrão com inicialização de nome 
+    ~Bureaucrat(void); // destrutor
+    Bureaucrat(const Bureaucrat &other); // construtor de cópia
+    Bureaucrat &operator=(const Bureaucrat &other); // atribuição por cópia
+
+    // getters
+    std::string getName(void) const;
+    int getGrade(void) const;
+
+    // up or down the grade
+    void upGrade(void);
+    void downGrade(void);
+
+    // new method - check that the bureaucrat has signed the form
+    void        signForm(Form &form) const;
+
+    // execeptions
+
+    class GradeTooHighException: public std::exception
+    {
+        public:
+        virtual const char *what() const throw();
+    };
+
+    class GradeTooLowException: public std::exception
+    {
+        public:
+        virtual const char *what()const throw();
+    };
+};
+
+std::ostream &operator<<(std::ostream &osStream, Bureaucrat const &that);
+
+#endif
---
+++ module_05/ex02/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsantana <bsantana@student.42sp.org.br     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/10 14:08:04 by bsantana          #+#    #+#             */
+/*   Updated: 2024/10/14 10:09:22 by bsantana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/Bureaucrat.hpp"
+#include "includes/AForm.hpp"
+
+void validSituations(void)
+{
+    return ;
+}
+
+void testCopyConstructor(void)
+{
+    return ;
+}
+
+void invalidSituations(void)
+{
+    return ;
+}
+
+int main(void)
+{
+    return 0;
+}
