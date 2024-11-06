4270be0c83a2a0c9f64e858c6b509116908c6b6f
bsantana <biancasantana81@hotmail.com>
2024-11-06 11:21:21 -0300

    finish ex01

--- module_09/ex01/main.cpp
+++ module_09/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bsantana <bsantana@student.42sp.org.br     +#+  +:+       +#+        */
+/*   By: bsantana <bsantana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 14:54:05 by bsantana          #+#    #+#             */
-/*   Updated: 2024/11/05 16:34:06 by bsantana         ###   ########.fr       */
+/*   Updated: 2024/11/06 11:20:56 by bsantana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int main(int argc, char **argv)
 {
     if (argc != 2)
     {
-        std::cout << "Usage: ./RPN \"<expression>\"" << std::endl;
+        std::cout << BRIGHT_RED "Usage: ./RPN \"<expression>\"" RESET << std::endl;
         return (1);
     }
 
@@ -24,9 +24,6 @@ int main(int argc, char **argv)
     std::string input = argv[1];
 
     rpnCalculator.calculate(input);
-
-    // validar se há operandos e operadores o suficiente para realizar a operação (um número a mais que a quantidade de operadores)
-    // notação polonesa invertida -> começar a adicionar na pilha, verificando é operando -> adiciona na pilha, não é retira os dois últimos e faz a operação.
 
     return (0);
 }
